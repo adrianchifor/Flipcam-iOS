1deb4a2518b6106b07a746e3d5d1573beb28a173
Paweł Kupiec <paw.kupiec@gmail.com>
2015-12-05 20:22:09 +0000

    Recording button and logic for showing progress bar

--- Flipcam/Flipcam/ViewController.swift
+++ Flipcam/Flipcam/ViewController.swift
@@ -5,6 +5,10 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var activeIndicator: UIView!
     @IBOutlet weak var cameraPreview: PreviewView!
+    var participation:Participation?
+    var currentSegment:Segment?
+    var nextSegment:Segment?
+    
     var timer:NSTimer?
     var i = 0
     
@@ -26,14 +30,32 @@ class ViewController: UIViewController {
         session.commitConfiguration()
         session.startRunning()
         timer = NSTimer.scheduledTimerWithTimeInterval(0.05, target: self, selector: "updateProgress", userInfo: nil, repeats: true)
+        self.currentSegment = Segment()
+        self.currentSegment!.stop = Int(NSDate().timeIntervalSince1970) + 5
+        self.currentSegment!.start = Int(NSDate().timeIntervalSince1970)
+        RKObjectManager.sharedManager().getObjectsAtPath("/api/v1/session", parameters: ["lat": 0, "lng": 0], success: { (operation, result) -> Void in
+            self.participation = (result.array()[0] as! Participation)
+            self.getNextSegment()
+            }) { (request, error) -> Void in
+                
+        }
 
     }
-    func updateProgress(){
-        progressView.setProgress(calculateProgress(), animated: true)
+    func getNextSegment() {
+        RKObjectManager.sharedManager().getObjectsAtPath("/api/v1/segment", parameters: ["key": self.participation!.key], success: { (operation, result) -> Void in
+            self.nextSegment = (result.array()[0] as! Segment)
+            
+            }) { (request, error) -> Void in
+                
+        }
     }
-    func calculateProgress() -> Float{
-        i++
-        return Float(i%101)/100.0
+    func updateProgress(){
+        progressView.setProgress(calculateProgress(self.currentSegment!), animated: true)
+    }
+    func calculateProgress(let segment:Segment) -> Float{
+        let current = NSDate().timeIntervalSince1970
+        let progress = min((Float(current - Double(segment.start)))/Float(segment.stop - segment.start), 1.0)
+        return progress
     }
     func setupActiveIndicator() {
         activeIndicator.layer.cornerRadius = activeIndicator.frame.width/2
@@ -54,6 +76,8 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+    
 
 
 }
