3316cb676233efa9886df2031f311695821fcbcd
Paweł Kupiec <paw.kupiec@gmail.com>
2015-12-05 16:59:05 +0000

    RestKit. Video preview. Flashing record indicator and progres bar.

---
+++ Flipcam/Flipcam/PreviewView.swift
@@ -0,0 +1,21 @@
+import AVFoundation
+import UIKit
+
+class PreviewView: UIView {
+    
+    var session:AVCaptureSession?{
+        get{
+            let prevLayer = layer as! AVCaptureVideoPreviewLayer
+            return prevLayer.session
+        }
+        set {
+            let prevLayer = layer as! AVCaptureVideoPreviewLayer
+            prevLayer.session = newValue
+        }
+    }
+    
+    override class func layerClass() -> AnyClass{
+        return AVCaptureVideoPreviewLayer.self
+    }
+    
+}
--- Flipcam/Flipcam/ViewController.swift
+++ Flipcam/Flipcam/ViewController.swift
@@ -1,20 +1,55 @@
-//
-//  ViewController.swift
-//  Flipcam
-//
-//  Created by Adrian Chifor on 05/12/2015.
-//  Copyright © 2015 SorryTeam. All rights reserved.
-//
-
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var activeIndicator: UIView!
+    @IBOutlet weak var cameraPreview: PreviewView!
+    var timer:NSTimer?
+    var i = 0
+    
+    @IBOutlet weak var progressView: UIProgressView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+        setupActiveIndicator()
+        startActiveIndicator()
+        
+        let session = AVCaptureSession()
+        let device = AVCaptureDevice.devicesWithMediaType(AVMediaTypeVideo)[0] as! AVCaptureDevice
+        let input = try! AVCaptureDeviceInput(device: device)
+        session.beginConfiguration()
+        session.addInput(input)
+        cameraPreview.session = session
+        let previewLayer = cameraPreview.layer as! AVCaptureVideoPreviewLayer
+        previewLayer.connection.videoOrientation = AVCaptureVideoOrientation.LandscapeLeft
+        session.commitConfiguration()
+        session.startRunning()
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.05, target: self, selector: "updateProgress", userInfo: nil, repeats: true)
 
+    }
+    func updateProgress(){
+        progressView.setProgress(calculateProgress(), animated: true)
+    }
+    func calculateProgress() -> Float{
+        i++
+        return Float(i%101)/100.0
+    }
+    func setupActiveIndicator() {
+        activeIndicator.layer.cornerRadius = activeIndicator.frame.width/2
+        activeIndicator.layer.opacity = 0
+    }
+    
+    func startActiveIndicator() {
+        activeIndicator.layer.opacity = 0
+        UIView.animateWithDuration(1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 2, options: [.Repeat, .Autoreverse], animations: { () -> Void in
+                self.activeIndicator.layer.opacity = 1
+            }) { (completed) -> Void in
+
+        }
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
